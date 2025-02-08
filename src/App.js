43462d8505a58fa74672ff0de22d667ba8a99c2d
iqsyal21 <iqsyal21@gmail.com>
2025-02-08 22:32:01 +0700

    change ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>Welcome to React 🚀</h1>
+      </header>
+
+      <p className="intro">
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+
+      <button className="btn">Get Started</button>
+    </div>
+  );
+};
 
 export default App;
